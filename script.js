373f325ee88ca5d3fb8a97115c107b38ef7247f7
sreejithkr047 <sreejithkr047@gmail.com>
2023-10-16 03:05:42 +0530

    Ui added
    2 buttons are used and dom manipulation is used to make the application more interactive

--- script.js
+++ script.js
@@ -6,46 +6,44 @@ function getcomputerchoice()
 
 function playround(playerselection,computerselection)
 {
-    playerselection=playerselection.toLowerCase();
+   
+    
     if(playerselection=="rock" && computerselection=="paper")
-    {
+    {       
         return 0;
     }
 
     else if(playerselection=="paper" && computerselection=="rock")
-    {
+    {   
         return 1;
     }
 
     else if(playerselection=="rock" && computerselection=="scissors")
-    {
+    {   
         return 1;
     }
 
     else if(playerselection=="scissors" && computerselection=="rock")
-    {
+    {   
         return 0;
     }
 
     else if(playerselection=="paper" && computerselection=="scissors")
-    {
+    {   
         return 0;
     }
     
     else if(playerselection=="scissors" && computerselection=="paper")
-    {
+    {   
         return 1;
     }
 
     else if(playerselection==computerselection)
-    {
+    {   
         return 2;
     }
 
-    else
-    {
-        return 3;
-    }
+   
     
     
 }
@@ -55,39 +53,95 @@ function Game()
     let playerselection;
     let computerselection;
     let result;
-
-    for (let i=0;i<5;i++)
-{   
-    console.log("Round "+(i+1)+"\t");
-    playerselection=prompt("Rock, Paper or scissors ?");
-    computerselection=getcomputerchoice();
-    result=playround(playerselection,computerselection);
+    let playercount=0;
+    let computercount=0;
     
+   
+   
+    const buttons=document.querySelectorAll('button');
+    buttons.forEach( button =>
+    {  
+       
+        button.addEventListener('click',()=>
+        {
+            playerselection=button.getAttribute('id');   
+            computerselection=getcomputerchoice();
+            result=playround(playerselection,computerselection);
 
-    if(result==1)
-    {
-        console.log("You win! "+playerselection+" beats "+computerselection+"\t");
-    }
+                        
+            if(result==1)
+            {
+                let display=document.querySelector('#display');
+                display.textContent="You win! "+playerselection+" beats "+computerselection+"\t";
+                playercount+=1;
+            }
 
-    else if(result==0)
-    {
-        console.log("You lose! "+computerselection+" beats "+playerselection+"\t");
-    }
+            else if(result==0)
+            {
+                
+                let display=document.querySelector('#display');
+                display.textContent="You lose! "+computerselection+" beats "+playerselection+"\t";
+                computercount=computercount+1;
+            }
 
-    else if(result==2)
-    {
-        console.log("ITS A DRAW! \t");
-    }
+            else if(result==2)
+            {
+               
+                let display=document.querySelector('#display');
+                display.textContent="ITS A DRAW";
+            }
 
-    else
-    {
-        console.log("Wrong input! \t")
-    }
+            const player=document.querySelector('.player span');
+            player.textContent=playercount;
+
+            const computer=document.querySelector('.computer span');
+            computer.textContent=computercount;
+
+            if(playercount==5 || computercount==5)
+            {   
+               
+                const winner=document.querySelector('.winner span');
+                winner.textContent=playercount==5?"Player wins the game!":"Computer wins the game!";
+            
+                display.style.display = 'none';
+                const score=document.querySelector('.score');
+                score.style.display = 'none';
+
+                const play=document.querySelector('.play');
+                play.innerHTML='<p>DO you wanna play again?</p> <button class="yes">YES</button> <button class="no">NO</button>';
+
+                const yes=document.querySelector('.yes');
+                const no=document.querySelector('.no');
+
+                yes.addEventListener('click',()=>
+                {   
+                    display.textContent='';
+                    playercount=0;
+                    computercount=0;
+                    player.textContent=playercount;
+                    computer.textContent=computercount;
+                    winner.textContent='';
+                   
+                    display.style.display = 'block';
+                    score.style.display = 'block';
+
+                });
+
+                no.addEventListener('click',()=>
+                {
+                play.remove();
+                    
+                });
+
+            }
+            
+            });
+    
+    });
+
 }
 
-}
 
 Game();
-
 
 
